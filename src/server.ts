f3ae623bd32722845d691a4ede4ae9e96e126c21
devruso <jamilsonjr@live.com>
2026-06-14 20:53:01 -0300

    fix(api): remove sensitive db config logging

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ const env = process.env.NODE_ENV || 'local';
 
 getConnectionOptions()
     .then(async options => {
-        console.log({ options });
         const extra = env !== 'local' ? { ssl: { rejectUnauthorized: false } } : undefined;
         return createConnection({ ...options, extra, migrationsRun: true });
     })
