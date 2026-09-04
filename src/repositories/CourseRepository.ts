ec1f30bbbff48cf0750992cae9130c45fb0f60d5
devruso <jamilsonjr@live.com>
2026-09-04 16:13:31 -0300

    feat: creates course repository

---
+++ src/repositories/CourseRepository.ts
@@ -0,0 +1,8 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import { Course } from '../entities/Course';
+
+@EntityRepository(Course)
+class CourseRepository extends Repository<Course> { }
+
+export { CourseRepository };
