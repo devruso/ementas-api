305bdea51f0fe7ef37ec9d569659977700244134
devruso <jamilsonjr@live.com>
2026-09-04 16:11:42 -0300

    feat: standardize academic level

--- src/interfaces/AcademicLevel.ts
+++ src/interfaces/AcademicLevel.ts
@@ -3,3 +3,12 @@ export enum AcademicLevel {
     MASTERS = 'mestrado',
     DOCTORATE = 'doutorado'
 }
+
+export const POST_GRADUATION_ACADEMIC_LEVEL = 'pos_graduacao' as const;
+
+export type ComponentAcademicLevel = AcademicLevel | typeof POST_GRADUATION_ACADEMIC_LEVEL;
+
+export const COMPONENT_ACADEMIC_LEVELS = [
+    ...Object.values(AcademicLevel),
+    POST_GRADUATION_ACADEMIC_LEVEL,
+] as const;
